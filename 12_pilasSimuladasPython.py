42c2c0b8706666890651adcf267a726786620e73
anbreaker <antunez19@gmail.com>
2019-10-06 21:38:01 +0200

    Work with set and dict

---
+++ 12_pilasSimuladasPython.py
@@ -0,0 +1,13 @@
+# Pilas Lifo Simuladas
+
+pila = [1, 2, 3]
+
+# Agregando elementos por el final
+pila.append(4)
+pila.append(5)
+print(pila)
+
+# Sacando elementos por el final .pop() Tambien retorna el valor que extrae
+n = pila.pop()
+print(f'Sacando el elemento --> {n}')
+print(pila)
