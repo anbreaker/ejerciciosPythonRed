c2c41a6da320051b7c47976ef89ad234f861f68d
anbreaker <antunez19@gmail.com>
2019-10-19 19:06:22 +0200

    Included test quantity

--- 17_ivaPaises.py
+++ 17_ivaPaises.py
@@ -1,20 +1,21 @@
 # Aplicando IVA general por paises:
 # Según el pais se aplica un tipo general de IVA u otro.
 # Haz un programa que aplique el tipo de IVA adecuado según el pais de origen
-#     Seguir la siguiente tabla:
-#         https://getquipu.com/blog/wp-content/uploads/2014/08/Artboard-asypv.png
-#         El programa debe mostrar la base, el iva aplicado en % y en € y el precio final.\n","\n","
-#         # Restricciones \n","1. Las cantidades en € deben aparecer ajustadas al céntimo\n","
-#         # 2. Utilizar una sola instrucción de impresión de resultados
+#   Seguir la siguiente tabla:
+#     https://getquipu.com/blog/wp-content/uploads/2014/08/Artboard-asypv.png
+#     El programa debe mostrar la base, el iva aplicado en % y en € y el precio final.
+#       # Restricciones: Las cantidades en € deben aparecer ajustadas al céntimo.
+#       # Utilizar una sola instrucción de impresión de resultados
 
 _POSIBLE_VALOR_ENTRADA = 'áéíóúüçÇÁÉÍÓÚ'
 _SALIDA = 'aeiouuzZAEIOU'
 
 ivaPaises = {
-    'Hungria': 27, 'Croacia': 25, 'Dinamarca': 25, 'Suecia': 25, 'Finlandia': 24, 'Rumania': 24,
-    'Grecia': 23, 'Irlanda': 23, 'Polonia': 23, 'Portugal': 23, 'Eslovenia': 22, 'Italia': 22,
-    'España': 21, 'Belgica': 21, 'Letonia': 21, 'Lituania': 21, 'Paises Bajos': 21, 'Republica Checa': 21,
-    'Austria': 20, 'Bulgaria': 20, 'Eslovaquia': 20, 'Estonia': 20, 'Francia': 20,
+    'Hungria': 27, 'Croacia': 25, 'Dinamarca': 25, 'Suecia': 25, 'Finlandia': 24,
+    'Rumania': 24, 'Grecia': 23, 'Irlanda': 23, 'Polonia': 23, 'Portugal': 23,
+    'Eslovenia': 22, 'Italia': 22, 'España': 21, 'Belgica': 21, 'Letonia': 21,
+    'Lituania': 21, 'Paises Bajos': 21, 'Republica Checa': 21, 'Austria': 20,
+    'Bulgaria': 20, 'Eslovaquia': 20, 'Estonia': 20, 'Francia': 20,
     'Reino Unido': 20, 'Alemania': 19, 'Chipre': 19, 'Malta': 18, 'Luxemburgo': 15
 }
 
@@ -29,9 +30,22 @@ def convertirAcentosTitle(nombre):
 def existePais(pais):
     pais = convertirAcentosTitle(pais)
     if pais in ivaPaises:
+        print(f'Valor del iva en {pais}--> {ivaPaises[pais]}')
         return True
     else:
         return False
+
+
+def validarCantidad(cantidad):
+    try:
+        if cantidad.isdigit() and float(cantidad) > 0:
+            return True
+    except ValueError:
+        return False
+
+
+def calcularIVA(pais):
+    pass
 
 
 # Pedir País
@@ -41,6 +55,12 @@ while not existePais(pais):
     # print(f'{ivaPaises.keys()}')
     pais = input('Escribe el pais para el que deseas saber el IVA: ')
     existePais(pais)
+
+# Introducir cantidad para pasar IVA
+cantidad = input('Introduce cantidad a aplicar IVA: ')
+while not validarCantidad(cantidad):
+    print('Debes introducir una cantidad numérica y positiva. ')
+    cantidad = input('Introduce cantidad a aplicar IVA: ')
 
 
 ver = convertirAcentosTitle(pais)
