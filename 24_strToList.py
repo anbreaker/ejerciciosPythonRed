b019d0fd9021120023d9c82c1d28caa51c1ffbec
anbreaker <antunez19@gmail.com>
2020-02-25 23:51:55 +0100

    Trabajar con listas en python3

--- 24_strToList.py
+++ 24_strToList.py
@@ -1,43 +1,43 @@
 # unir listas (+)
 lista = [1, 2, 3]
 
-listaDos = [4, 5, 6]
-listaTres = lista + listaDos
-lista.extend(listaDos)
+lista_2 = [4, 5, 6]
+lista_3 = lista + lista_2
+lista.extend(lista_2)
 
-print(f'Unir listas: {listaTres}')
+print(f'Unir listas: {lista_3}')
 print(f'Unir listas: {lista}')
 
 # Multiplicar elementos
-listaCuatro = [5]
-listaCinco = listaCuatro * 10
-print(f'multiplicar elementos {listaCinco}')
+lista_4 = [5]
+lista_5 = lista_4 * 10
+print(f'multiplicar elementos {lista_5}')
 """los operadores /(division) y -(resta) no puede utilizarse en cuando tenemos el tipo list
 las listas soportan el operador de slices [:::]. punto de inicio, punto final y opcional de cada cuantos pasos."""
 
 # indice uno hasta 3
-listaSeis = listaTres[1:3]
-print(f'index 1 hasta el 3: {listaSeis}')
+lista_6 = lista_3[1:3]
+print(f'index 1 hasta el 3: {lista_6}')
 
 # indice uno hasta 3 de 2 en 2
-listaSiete = listaTres[0:6:2]
-print(f'index 0 hasta el 6 de 2 en 2: {listaSiete}')
+lista_7 = lista_3[0:6:2]
+print(f'index 0 hasta el 6 de 2 en 2: {lista_7}')
 
 # desde el inicio hasta el final invertida
-listaOcho = listaTres[::-1]
-print(f'Desde el inicio hasta el final invertida: {listaOcho}')
+lista_8 = lista_3[::-1]
+print(f'Desde el inicio hasta el final invertida: {lista_8}')
 
 # anadir elemento al final de la lista
-listaOcho.append(9)
-print(f'Añadir elememto{listaOcho}')
+lista_8.append(9)
+print(f'Añadir elememto{lista_8}')
 
 # Eliminar ultimo elemento de la lista y lo devuelve
-numero_devuelto = listaOcho.pop()
-print(f'Eliminar ultimo elememto{listaOcho}, el numero devuelto es: {numero_devuelto}')
+numero_devuelto = lista_8.pop()
+print(f'Eliminar ultimo elememto{lista_8}, el numero devuelto es: {numero_devuelto}')
 
 # Elimina el elemento de la poscion 'x' de la lista y lo devuelve
-elemento_eliminado = listaOcho.pop(2)
-print(f'Eliminar elememto selecionado {listaOcho}, el elemento eliminado es {elemento_eliminado}')
+elemento_eliminado = lista_8.pop(2)
+print(f'Eliminar elememto selecionado {lista_8}, el elemento eliminado es {elemento_eliminado}')
 
 listaDesordenada = [5, 7, 9, 2, 1, 4, 3]
 listaDesordenada.sort()
